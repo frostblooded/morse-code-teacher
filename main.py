f8e363e1740172494e7c01e2595c92f31390c1a9
Nikolay Danailov <frostblooded@yahoo.com>
2017-04-14 15:23:00 +0300

    Refactor code a bit into functions

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ import time
 INPUT_BUTTON_PIN = 17
 END_INPUT_BUTTON_PIN = 18
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(INPUT_BUTTON_PIN, GPIO.IN)
-GPIO.setup(END_INPUT_BUTTON_PIN, GPIO.IN)
+if __name__ == '__main__':
+    init()
 
-while True:
-    if GPIO.input(INPUT_BUTTON_PIN):
-        print('Input button pressed!')
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(INPUT_BUTTON_PIN, GPIO.IN)
+    GPIO.setup(END_INPUT_BUTTON_PIN, GPIO.IN)
 
-    if GPIO.input(END_INPUT_BUTTON_PIN):
-        print('End input button pressed!')
+    while True:
+        loop()
+
+def loop():
+    read_input()
+
+def read_input():
+    while True:
+        if GPIO.input(INPUT_BUTTON_PIN):
+            print('Input button pressed!')
+
+        if GPIO.input(END_INPUT_BUTTON_PIN):
+            print('End input button pressed!')
